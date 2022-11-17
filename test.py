dd579f3e5f4a6e86bdc46aaa401595f0253bce9b
Fran <fran.rosillo.10@gmail.com>
2022-11-17 18:10:24 +0100

    Función intersectar poligonos

---
+++ test.py
@@ -0,0 +1,6 @@
+from python.modulos.intersecciones import interseccion_poligonos
+
+file1 = 'datos_ini/barris-barrios.geojson'
+file2 = 'datos_ini/lday_tota.json'
+
+interseccion_poligonos(file1, file2)
