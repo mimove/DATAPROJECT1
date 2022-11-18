7e26f1614a4cd0e1e3d2573b104147219cb612e7
Fran <fran.rosillo.10@gmail.com>
2022-11-18 20:17:47 +0100

    Por estación de contaminación mostramos la calidad

--- python/main.py
+++ python/main.py
@@ -35,7 +35,7 @@ barrios_gpd = gpd.GeoDataFrame.from_features(barrios_json)
 barrios_gpd.crs = 'epsg:4326' #Aseguramos que la proyección es la adecuada para coordenadas GPS
 barrios_gpd = barrios_gpd.rename(columns ={'nombre':'nombre_barrio','codbarrio':'codigo_barrio'}) 
 
-
+''''''
 barrios_updated = varinter.interseccion_poligonos(barrios_gpd, dir_datos_ini + 'zonas-verdes.geojson', 'area', '','%_zona_verde')
 
 
@@ -47,16 +47,29 @@ barrios_updated = varinter.interseccion_poligonos(barrios_updated, dir_datos_ini
 
 # Cálculo número de hospitales por barrio
 
-barrios_updated = varinter.interseccion_puntos(barrios_updated,  dir_datos_ini + 'hospitales.geojson','num_hospitales' )
+barrios_updated = varinter.interseccion_puntos(barrios_updated,  dir_datos_ini + 'hospitales.geojson','num_hospitales', 'points' )
 
 
 # Cálculo número de centros educativos por barrios
 
-barrios_updated = varinter.interseccion_puntos(barrios_updated,  dir_datos_ini + 'centros-educativos-en-valencia.geojson','num_colegios' )
+barrios_updated = varinter.interseccion_puntos(barrios_updated,  dir_datos_ini + 'centros-educativos-en-valencia.geojson','num_colegios', 'points')
 
+ # Cálculo número de puntos de carga para coche eléctrico
 
+barrios_updated = varinter.interseccion_puntos(barrios_updated,  dir_datos_ini + 'carregadors-vehicles-electrics-cargadores-vehiculos-electricos.geojson','num_chargestations', 'points')
 
+ # Cáculo número de estaciones de contaminación
 
+barrios_updated = varinter.interseccion_puntos(barrios_updated,  dir_datos_ini + 'estacions-contaminacio-atmosferiques-estaciones-contaminacion-atmosfericas.geojson', 'polution_stations', 'quality')
+
+ # Cálculo número de contenedores de residuos por barrio
+
+barrios_updated = varinter.interseccion_puntos(barrios_updated,  dir_datos_ini + 'contenidors-residus-solids-contenidores-residuos-solidos.geojson', 'num_contenedores', 'points')
+
+ # Cálculo del numero de papeleras por barrio
+
+barrios_updated = varinter.interseccion_puntos(barrios_updated,  dir_datos_ini + 'papereres-papeleras.geojson', 'num_papeleras', 'points')
+ 
 with open(dir_datos_out + "barrios_updated.geojson", "w") as outfile:  #Generamos archivo geojson con el porventaje de intersección de cada barrio
         outfile.write(barrios_updated.to_json())
 
