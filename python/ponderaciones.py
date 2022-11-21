ebcae1004e5044e95531a23eba451cec3e16e348
darydaro <100683177+fernandezdario@users.noreply.github.com>
2022-11-21 20:02:17 +0100

    Update responses

--- python/ponderaciones.py
+++ python/ponderaciones.py
@@ -3,7 +3,7 @@ from pydrive.drive import GoogleDrive
 from oauth2client import client, file, tools
 import pandas as pd
   
-'''# Initializing a GoogleAuth Object
+# Initializing a GoogleAuth Object
 gauth = GoogleAuth()
 
 # client_secrets.json file is verified
@@ -19,14 +19,11 @@ creds = store.get()
 # Initialize GoogleDriveFile instance with file id
 file_obj = drive.CreateFile({'id': '15zDrSsGwwX3Kj0oZCPhUPxr0WyRUl1vku6esoxJA7n4'})
 file_obj.GetContentFile('responses.xls',
-         mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')'''
+         mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
 
-dataframe = pd.read_excel('responses.xls')
-print(dataframe)
+'''dataframe = pd.read_excel('responses.xls')
+print(dataframe)'''
 
-workbook = pd.ExcelFile('responses.xls')
-d = {} # start with an empty dictionary
-for sheet_name in workbook.sheet_names:
-    df = workbook.parse(sheet_name)
-    d[sheet_name] = df
+
+
 
